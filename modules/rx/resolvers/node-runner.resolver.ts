d9eebba21f43f35da676ef9c8fe5f096af9a5e90
Alexandr Plokhih <s.plohoj@gmail.com>
2020-06-13 21:30:21 +0300

    Type definition fix

--- modules/rx/resolvers/node-runner.resolver.ts
+++ modules/rx/resolvers/node-runner.resolver.ts
@@ -1,14 +1,14 @@
 import { NodeAndLocalRunnerResolverBase, ResolvedRunnerArguments, RunnerConstructor } from '@worker-runner/core';
 import { RX_WORKER_RUNNER_ERROR_SERIALIZER } from '../errors/error.serializer';
 import { RxRunnerController } from '../runners/runner.controller';
-import { IRxRunnerSerializedParameter, RxResolvedRunner } from '../types/resolved-runner';
+import { IRxRunnerSerializedParameter, RxResolvedRunner, RxResolvedRunnerArguments } from '../types/resolved-runner';
 
 export class RxNodeRunnerResolver<R extends RunnerConstructor> extends NodeAndLocalRunnerResolverBase<R> {
 
     declare public resolve: <RR extends R>(
         runner: RR,
         ...args: RR extends new (...args: infer A) => any ?
-            A extends Array<IRxRunnerSerializedParameter> ? ResolvedRunnerArguments<A> : never : never
+            A extends Array<IRxRunnerSerializedParameter> ? RxResolvedRunnerArguments<A> : never : never
     ) => Promise<RxResolvedRunner<InstanceType<RR>>>;
 
     declare protected runnerControllers: Set<RxRunnerController<R>>;
